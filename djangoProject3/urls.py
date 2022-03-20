1c927a763688cd2a1cdf41a01c70c98240abb654
bugrataskinyalcin <bugrayalcin62@gmail.com>
2022-03-20 07:26:55 +0300

    token passed

--- djangoProject3/urls.py
+++ djangoProject3/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mylobby.views import home_view
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 ]
